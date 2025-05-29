0274ba0cf5d91f3200ab4e10eb7b0ede5f9f81ec
Luis <luisbarrios0110@gmail.com>
2025-05-28 20:39:26 -0500

    Feat: Agregar información y diseño al componente ProductCard y actualizar la sección de productos

--- src/data.ts
+++ src/data.ts
@@ -10,10 +10,25 @@ type Categorias = "amigurumis" | "bolsos" | "decoracion"
 
 export const productos: Producto[] = [
   {
-    nombre: "Bluey",
-    foto: "bluey.webp",
+    nombre: "Bunny",
+    foto: "bunny.webp",
     tamaño: 6,
-    descripcion: "Tierno osito de crochet, ideal como regalo para niños y bebés.",
+    descripcion: "Tierno conejito de crochet, ideal como regalo de san valentin!",
+    /* descripcion: "Tierno osito de crochet, ideal como regalo para niños y bebés.", */
+    categotia: "amigurumis"
+  },
+  {
+    nombre: "Bolso de tira",
+    foto: "bolso-camila.webp",
+    tamaño: 10,
+    descripcion: "Bolso de tira tejido a mano, ideal para llevar tus cosas con estilo.",
+    categotia: "bolsos"
+  },
+  {
+    nombre: "Stitch & Angel",
+    foto: "stitch.webp",
+    tamaño: 4,
+    descripcion: "Pareja de amigurumis de Stitch y Angel, perfectos para los amantes de Disney.",
     categotia: "amigurumis"
   }
 ] 
