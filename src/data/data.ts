c12737f8f72a7d75e8f2c82e0ea725d41696aad9
OutziderDev <luisbarrios0110@gmail.com>
2026-03-16 22:26:10 -0500

    feat: agregar nuevo producto "Hou el Travieso" con imagen y detalles

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,229 +1,265 @@
-import type { Producto } from "../types/Producto"
+import type { Producto } from "../types/Producto";
 
 export const productos: Producto[] = [
+  {
+    nombre: "Hou el Travieso",
+    foto: "mono-zodiaco.webp",
+    tamaño: 5,
+    descripcion:
+      "El mono más travieso del zodiaco chino, símbolo de ingenio, alegría y picardía: ¡el adorable Hou! 🐒✨",
+    categoria: "Amigurumis",
+    precio: 10.0,
+  },
   {
     nombre: "Link de Zelda",
     foto: "link-ia.webp",
     tamaño: 6,
     descripcion: "El héroe de Hyrule ha llegado: ¡el adorable Link de The Legend of Zelda! 🗡️✨",
     categoria: "Amigurumis",
-    precio: 10.00
+    precio: 10.0,
   },
   {
     nombre: "Himura Kenshin",
     foto: "kenshin-batusai.webp",
     tamaño: 6,
-    descripcion: "El legendario espadachín de la era Meiji ha llegado: ¡el adorable Himura Kenshin de Samurai X! 🗡️✨",
+    descripcion:
+      "El legendario espadachín de la era Meiji ha llegado: ¡el adorable Himura Kenshin de Samurai X! 🗡️✨",
     categoria: "Amigurumis",
-    precio: 16.00
+    precio: 16.0,
   },
   {
     nombre: "Ramo de Tulipanes",
     foto: "ramo-tulipanes.webp",
     tamaño: 8,
-    descripcion: "Ramo de tulipanes tejidos a mano, perfectos para regalar o decorar tu hogar con un toque de color y alegría. 🌷✨",
+    descripcion:
+      "Tulipanes tejidos a mano, perfectos para regalar o decorar tu hogar con un toque de color y alegría. 🌷✨",
     categoria: "Accesorios",
-    precio: 16.00,
+    precio: 16.0,
   },
   {
     nombre: "Snoopy y Emilio",
     foto: "snoopy-emilio.webp",
     tamaño: 4,
-    descripcion: "Snoopy y su amigo Emilio, tejidos a mano con amor y detalle, para llenar de ternura tu espacio. 🐶💖",
+    descripcion:
+      "Snoopy y su amigo Emilio, tejidos a mano con amor y detalle, para llenar de ternura tu espacio. 🐶💖",
     categoria: "Accesorios",
-    precio: 8.00,
+    precio: 8.0,
   },
   {
     nombre: "Bolso de Verano",
     foto: "bolso-verano.webp",
     tamaño: 10,
-    descripcion: "Bolso tejido a mano, ideal para llevar tus accesorios en verano con estilo y frescura. ☀️🌊",
+    descripcion:
+      "Bolso tejido a mano, ideal para llevar tus accesorios en verano con estilo y frescura. ☀️🌊",
     categoria: "Bolsos",
-    precio: 15.00,
+    precio: 15.0,
   },
   {
     nombre: "Patitos Enamorados",
     foto: "patitos-unidos.webp",
     tamaño: 2,
-    descripcion: "Patitos enamorados que siempre se acompañan, ideales para decorar tus llaves o mochila. 🦆💕",
+    descripcion:
+      "Patitos enamorados que siempre se acompañan, ideales para decorar tus llaves o mochila. 🦆💕",
     categoria: "Accesorios",
-    precio: 8.00,
+    precio: 8.0,
   },
   {
     nombre: "Baby Angels",
     foto: "twin-angels.webp",
     tamaño: 6,
-    descripcion: "Dulces par de gemelos tejidos a mano, perfectos para decorar tus eventos más especiales. 👼✨",
+    descripcion:
+      "Dulces par de gemelos tejidos a mano, perfectos para decorar tus eventos más especiales. 👼✨",
     categoria: "Amigurumis",
-    precio: 25.00,
+    precio: 25.0,
   },
   {
     nombre: "Osito Personalizado",
     foto: "arreglo-mathias.webp",
     tamaño: 12,
-    descripcion: "Osito con arreglo personalizado, perfecto para decorar tu espacio con estilo y ternura. 🧸💕",
+    descripcion:
+      "Osito con arreglo personalizado, perfecto para decorar tu espacio con estilo y ternura. 🧸💕",
     categoria: "Amigurumis",
-    precio: 20.00,
+    precio: 20.0,
   },
   {
     nombre: "Teddy Bear",
     foto: "teddy-mrbean.webp",
     tamaño: 6,
-    descripcion: "El osito de Mr Bean, tierno y abrazable, para que siempre tengas un amigo a tu lado. 🧸💕",
+    descripcion:
+      "El osito de Mr Bean, tierno y abrazable, para que siempre tengas un amigo a tu lado. 🧸💕",
     categoria: "Amigurumis",
-    precio: 10.00,
+    precio: 10.0,
   },
   {
     nombre: "Virgen Santa Librada",
     foto: "santa-librada.webp",
     tamaño: 4,
-    descripcion: "Protectora de causas difíciles, ahora en llavero amigurumi para brindarte su bendición y apoyo. 🙏✨",
+    descripcion:
+      "Protectora de causas difíciles, ahora en llavero amigurumi para brindarte su bendición y apoyo. 🙏✨",
     categoria: "Accesorios",
-    precio: 8.00,
+    precio: 8.0,
   },
   {
     nombre: "Angelitos",
     foto: "angelitos.webp",
     tamaño: 6,
-    descripcion: "Dulces angelitos tejidos a mano, perfectos para decorar tus eventos más especiales. 👼✨",
+    descripcion:
+      "Dulces angelitos tejidos a mano, perfectos para decorar tus eventos más especiales. 👼✨",
     categoria: "Amigurumis",
-    precio: 12.00,
+    precio: 12.0,
   },
   {
     nombre: "Virgen de Guadalupe",
     foto: "virgen-guadalupe.webp",
     tamaño: 6,
-    descripcion: "La Virgen de Guadalupe, emblema de amor y consuelo, ahora en amigurumi para proteger tu hogar. ✨🌹🙏",
+    descripcion:
+      "La Virgen de Guadalupe, emblema de amor y consuelo, ahora en amigurumi para proteger tu hogar. ✨🌹🙏",
     categoria: "Amigurumis",
-    precio: 17.00,
+    precio: 17.0,
   },
   {
     nombre: "Cosito, el Osito",
     foto: "cosito.webp",
     tamaño: 8,
-    descripcion: "El osito más tierno y abrazable, hecho a mano con amor e hilo peludo, para llenar de dulzura tu vida. 🧸💕",
+    descripcion:
+      "El osito más tierno y abrazable, hecho a mano con amor e hilo peludo, para llenar de dulzura tu vida. 🧸💕",
     categoria: "Amigurumis",
-    precio: 12.00,
+    precio: 12.0,
   },
   {
     nombre: "Pally, el Pati Pollito",
     foto: "pallyto.webp",
     tamaño: 4,
-    descripcion: "¡Conoce a Pally, el pato pollito más adorable y divertido que alegrará tus días con su ternura! 🦆🐥",
+    descripcion:
+      "¡Conoce a Pally, el pato pollito más adorable y divertido que alegrará tus días con su ternura! 🦆🐥",
     categoria: "Amigurumis",
-    precio: 4.00
+    precio: 4.0,
   },
   {
     nombre: "Frankisito",
     foto: "frankisito.webp",
     tamaño: 6,
-    descripcion: "Una adorable fusión entre Frankenstein y Cositos: ¡dos almas tejidas en un solo amigurumi! 🐰🐶",
+    descripcion:
+      "Una adorable fusión entre Frankenstein y Cositos: ¡dos almas tejidas en un solo amigurumi! 🐰🐶",
     categoria: "Amigurumis",
-    precio: 15.00
+    precio: 15.0,
   },
   {
     nombre: "Virgen La Milagrosa",
     foto: "virgen-milagrosa.webp",
     tamaño: 8,
-    descripcion: "La Virgen Milagrosa, símbolo de fe y esperanza, ahora en un amigurumi tejido a mano con amor y devoción. 🙏✨",
+    descripcion:
+      "La Virgen Milagrosa, símbolo de fe y esperanza, ahora en un amigurumi tejido a mano con amor y devoción. 🙏✨",
     categoria: "Amigurumis",
-    precio: 17.00,
+    precio: 17.0,
   },
   {
     nombre: "Inosuke Hashibira",
     foto: "inosukechiby.webp",
     tamaño: 8,
-    descripcion: "El feroz cazador de demonios con máscara de jabalí aparece: ¡el adorable Inosuke de Kimetsu no Yaiba🐗!",
+    descripcion:
+      "El feroz cazador de demonios con máscara de jabalí aparece: ¡el adorable Inosuke de Kimetsu no Yaiba🐗!",
     categoria: "Accesorios",
-    precio: 13.00
+    precio: 13.0,
   },
   {
     nombre: "El Payaso Plim Plim",
     foto: "plimplim.webp",
     tamaño: 5,
-    descripcion: "El tierno payaso Plim Plim ha llegado para alegrar tus días con su sonrisa y encanto inigualable 🤡!",
+    descripcion:
+      "El tierno payaso Plim Plim ha llegado para alegrar tus días con su sonrisa y encanto inigualable 🤡!",
     categoria: "Amigurumis",
-    precio: 10.00
+    precio: 10.0,
   },
   {
     nombre: "Arbolito de Navidad",
     foto: "arbolito-navidad.webp",
     tamaño: 6,
-    descripcion: "Decora tu hogar con un toque artesanal con nuestro encantador arbolito de Navidad tejido a mano. 🎄✨",
+    descripcion:
+      "Decora tu hogar con un toque artesanal con nuestro encantador arbolito de Navidad tejido a mano. 🎄✨",
     categoria: "Accesorios",
-    precio: 10.00
+    precio: 10.0,
   },
   {
     nombre: "Shinobu Kocho",
     foto: "shinobu.webp",
     tamaño: 6,
-    descripcion: "La cazadora de insectos más elegante y letal ha llegado: ¡la encantadora Shinobu Kocho de Kimetsu no Yaiba🦋!",
+    descripcion:
+      "La cazadora de insectos más elegante y letal ha llegado: ¡la encantadora Shinobu Kocho de Kimetsu no Yaiba🦋!",
     categoria: "Amigurumis",
-    precio: 10.00
+    precio: 10.0,
   },
   {
     nombre: "Neko Bolso",
     foto: "nekobolso.webp",
     tamaño: 8,
-    descripcion: "Bolso tejido a mano con diseño de gato, ideal para llevar tus pertenencias con estilo y ternura.",
+    descripcion:
+      "Bolso tejido a mano con diseño de gato, ideal para llevar tus pertenencias con estilo y ternura.",
     categoria: "Bolsos",
-    precio: 10.00
+    precio: 10.0,
   },
   {
     nombre: "Kamado Nezuko",
     foto: "nezukoamigurumi.webp",
     tamaño: 8,
-    descripcion: "La gran cazadora de demonios ya está aquí: ¡la adorable Nezuko Kamado de Kimetsu no Yaiba👺!",
+    descripcion:
+      "La gran cazadora de demonios ya está aquí: ¡la adorable Nezuko Kamado de Kimetsu no Yaiba👺!",
     categoria: "Amigurumis",
-    precio: 15.00
+    precio: 15.0,
   },
   {
     nombre: "Monederos Personalizado",
     foto: "monedero.webp",
     tamaño: 4,
-    descripcion: "Monederos personalizados tejidos a mano, ideales para llevar tus monedas y pequeños objetos con estilo.",
+    descripcion:
+      "Monederos personalizados tejidos a mano, ideales para llevar tus monedas y pequeños objetos con estilo.",
     categoria: "Accesorios",
-    precio: 6.00
+    precio: 6.0,
   },
   {
     nombre: "Mini Draco",
     foto: "minidraco.webp",
     tamaño: 4,
-    descripcion: "El último de su linaje, reducido por magia antigua al tamaño de un llavero, para custodiar tus llaves.",
+    descripcion:
+      "El último de su linaje, reducido por magia antigua al tamaño de un llavero, para custodiar tus llaves.",
     categoria: "Accesorios",
-    precio: 10.00
+    precio: 10.0,
   },
   {
     nombre: "Son Goku - Jonbi",
     foto: "jonbi.webp",
     tamaño: 4,
-    descripcion: "El Jonbi, legendario Biju de 4 colas esta aquí para brindarte poder y acompañarte en cada aventura.",
+    descripcion:
+      "El Jonbi, legendario Biju de 4 colas esta aquí para brindarte poder y acompañarte en cada aventura.",
     categoria: "Amigurumis",
-    precio: 10.00
+    precio: 10.0,
   },
   {
     nombre: "Llavero de Chimuelo",
     foto: "chimuelollavero.webp",
     tamaño: 4,
-    descripcion: "Llaveros de chimuelo y furia diurna para que te acompañen y protejan en todo lugar",
+    descripcion:
+      "Llaveros de chimuelo y furia diurna para que te acompañen y protejan en todo lugar",
     categoria: "Accesorios",
-    precio: 5.00
+    precio: 5.0,
   },
   {
     nombre: "Dino Llaveros",
     foto: "dinollavero.webp",
     tamaño: 3,
-    descripcion: "Tierno par de dino llaveros que pueden compartir un tierno beso cada vez que los juntas.",
+    descripcion:
+      "Tierno par de dino llaveros que pueden compartir un tierno beso cada vez que los juntas.",
     categoria: "Accesorios",
-    precio: 10
+    precio: 10,
   },
   {
     nombre: `Bolso de Crochet 13"x11"`,
     foto: "bolso-13x11.webp",
     tamaño: 13,
-    descripcion: "Elegante bolso de 13x11 pulgadas, ideal para llevar tus artículos y lucir con estilo en cada viaje.",
+    descripcion:
+      "Elegante bolso de 13x11 pulgadas, ideal para llevar tus artículos y lucir con estilo en cada viaje.",
     categoria: "Bolsos",
-    precio: 15
+    precio: 15,
   },
   {
     nombre: "Spiderman",
@@ -231,7 +267,7 @@ export const productos: Producto[] = [
     tamaño: 4,
     descripcion: "Pequeño llavero inspirado en el super heroe Spider Man",
     categoria: "Amigurumis",
-    precio: 8.00
+    precio: 8.0,
   },
   {
     nombre: "Cinnamon ",
@@ -239,15 +275,16 @@ export const productos: Producto[] = [
     tamaño: 7,
     descripcion: "Suave y tierno amigurumi en Hilo Chenille inspirado en los amigos de hello kitty",
     categoria: "Amigurumis",
-    precio: 10.00
+    precio: 10.0,
   },
   {
     nombre: "Llavero de Garrafon ",
     foto: "garrafon.webp",
     tamaño: 3,
-    descripcion: "Llavero personalizado de un Garrafon de agua, ideal para acompañar tus llaves y darle un toque divertido.",
+    descripcion:
+      "Llavero personalizado de un Garrafon de agua, ideal para acompañar tus llaves y darle un toque divertido.",
     categoria: "Accesorios",
-    precio: 6.00
+    precio: 6.0,
   },
   {
     nombre: "Bunny",
@@ -255,7 +292,7 @@ export const productos: Producto[] = [
     tamaño: 6,
     descripcion: "Tierno conejito de crochet, ideal como regalo de san valentin!",
     categoria: "Amigurumis",
-    precio: 10.00
+    precio: 10.0,
   },
   {
     nombre: "Bolso de Tirante",
@@ -263,7 +300,7 @@ export const productos: Producto[] = [
     tamaño: 8,
     descripcion: "Elegante bolso de tirante que combina con toda ocasión",
     categoria: "Bolsos",
-    precio: 9.00
+    precio: 9.0,
   },
   {
     nombre: "Stitch & Angel",
@@ -271,23 +308,25 @@ export const productos: Producto[] = [
     tamaño: 4,
     descripcion: "Pareja de amigurumis de Stitch y Angel, perfectos para los amantes de Disney.",
     categoria: "Amigurumis",
-    precio: 8.00
+    precio: 8.0,
   },
   {
     nombre: "LLavero de Saxofón",
     foto: "saxofonllavero.webp",
     tamaño: 4,
-    descripcion: "Llavero personalizado de un saxofón, el compañero ideal para los amantes de la música.",
+    descripcion:
+      "Llavero personalizado de un saxofón, el compañero ideal para los amantes de la música.",
     categoria: "Accesorios",
-    precio: 8.00
+    precio: 8.0,
   },
   {
     nombre: "Ghost",
     foto: "ghost.webp",
     tamaño: 6,
-    descripcion: "Directo del universo de COD, este amigurumi te acompañara en todas las partidas y aventuras",
+    descripcion:
+      "Directo del universo de COD, este amigurumi te acompañara en todas las partidas y aventuras",
     categoria: "Amigurumis",
-    precio: 8.00
+    precio: 8.0,
   },
   {
     nombre: "Bluey",
@@ -295,7 +334,7 @@ export const productos: Producto[] = [
     tamaño: 8,
     descripcion: "Tierno osito de crochet, ideal como regalo para niños y bebés.",
     categoria: "Amigurumis",
-    precio: 13.00
+    precio: 13.0,
   },
   {
     nombre: "Bolso de tira",
@@ -303,8 +342,8 @@ export const productos: Producto[] = [
     tamaño: 10,
     descripcion: "Bolso de tira tejido a mano, ideal para llevar tus accesorios con estilo.",
     categoria: "Bolsos",
-    precio: 15.00
-  }
+    precio: 15.0,
+  },
   /* ,
   {
     nombre: "Osito Personalizado",
@@ -314,5 +353,4 @@ export const productos: Producto[] = [
     categoria: "Amigurumis",
     precio: 6
   }, */
-
-]
+];
